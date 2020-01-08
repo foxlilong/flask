ea741fea8f92baa7ced1efa554e19571fdf0cda1
oliver65510 <oliver65510@gmail.com>
2020-01-08 19:20:31 +0800

    templates

--- demo1/app1-helloworld/app/routes.py
+++ demo1/app1-helloworld/app/routes.py
@@ -1,5 +1,18 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 def index():
-    return "Hello world"
+    user = {'username': 'jack'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='index', user=user, posts=posts)
